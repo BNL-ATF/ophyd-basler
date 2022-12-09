22db13a223b1365f428bf31635c93cefb052a0f2
tom <tom@vpn12-client-172-20-216-110.princeton.edu>
2022-12-09 16:30:10 -0500

    changed test structure

---
+++ ophyd_basler/tests/conftest.py
@@ -0,0 +1,40 @@
+import datetime
+
+from bluesky.callbacks import best_effort
+from bluesky.run_engine import RunEngine
+from databroker import Broker
+from ophyd.utils import make_dir_tree
+from datetime import datetime
+
+from ophyd_basler.basler_handler import BaslerCamHDF5Handler
+
+import pytest
+
+@pytest.fixture(scope="function")
+def db():
+    """Return a data broker
+    """
+
+    db = Broker.named('temp')
+    db.reg.register_handler("BASLER_CAM_HDF5", BaslerCamHDF5Handler, overwrite=True)
+
+    return db
+
+
+@pytest.fixture(scope="function")
+def RE(db):
+
+    RE = RunEngine({})
+    bec = best_effort.BestEffortCallback()
+    bec.disable_plots()
+    RE.subscribe(bec)
+    RE.subscribe(db.insert)
+
+    return RE
+
+
+@pytest.fixture(scope="function")
+def make_dirs():
+
+    root_dir = '/tmp/basler'
+    _ = make_dir_tree(datetime.now().year, base_path=root_dir)
--- ophyd_basler/tests/test_emulated_camera.py
+++ ophyd_basler/tests/test_emulated_camera.py
@@ -1,39 +1,20 @@
+from ophyd_basler.basler_camera import BaslerCamera
+
+import os
+import numpy as np
+import bluesky.plans as bp
+
 import pytest
 
-def test_emulated_basler_camera():
-
-    from ophyd_basler.basler_camera import BaslerCamera
-    from ophyd_basler.basler_handler import BaslerCamHDF5Handler
-
-    import os
-    import numpy as np
-    import bluesky.plans as bp
-    from bluesky.callbacks import best_effort
-    from bluesky.run_engine import RunEngine
-    from databroker import Broker
-    from ophyd.utils import make_dir_tree
-    from datetime import datetime
+def test_emulated_basler_camera(RE, db):
 
     os.environ['PYLON_CAMEMU'] = "1"
 
-    root_dir = '/tmp/basler'
-    _ = make_dir_tree(datetime.now().year, base_path=root_dir)
-
-    RE = RunEngine({})
-    bec = best_effort.BestEffortCallback()
-    RE.subscribe(bec)
-
-    db = Broker.named('temp')
-
-    RE.subscribe(db.insert)
-
     emulated_basler_camera = BaslerCamera(cam_num=0)
 
-    db.reg.register_handler("BASLER_CAM_HDF5", BaslerCamHDF5Handler, overwrite=True)
-
-    uid, = RE(bp.count([emulated_basler_camera]))
+    uid, = RE(bp.count([emulated_basler_camera], num=3))
 
     hdr = db[uid]
 
-    assert np.array(list(hdr.data(field='basler_cam_image', fill=True))).shape == (1, 1040, 1024)
+    assert np.array(list(hdr.data(field='basler_cam_image', fill=True))).shape == (3, 1040, 1024)
 
