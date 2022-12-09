6c4a8e6bac984c13767e2e8243b44687c35fad0f
tom <tom@vpn12-client-172-20-216-110.princeton.edu>
2022-12-09 15:13:36 -0500

    CI

--- examples/prepare_basler_env.py
+++ examples/prepare_basler_env.py
@@ -1,6 +1,7 @@
 from ophyd_basler.basler_camera import BaslerCamera
 from ophyd_basler.basler_handler import BaslerCamHDF5Handler
 
+import os
 import numpy as np
 import bluesky.plans as bp
 from bluesky.callbacks import best_effort
@@ -21,14 +22,3 @@ RE.subscribe(bec)
 db = Broker.named('temp')
 
 RE.subscribe(db.insert)
-
-cam = BaslerCamera()
-
-db.reg.register_handler("BASLER_CAM_HDF5", BaslerCamHDF5Handler, overwrite=True)
-
-uid, = RE(bp.count([cam]))
-
-hdr = db[uid]
-
-print(np.array(list(hdr.data(field='basler_cam_image', fill=True))).shape)
-
