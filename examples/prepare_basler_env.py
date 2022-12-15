b6dc4346fa116222a4149398db0c066f7cfc13cf
Maksim Rakitin <mrakitin@bnl.gov>
2022-12-15 16:26:37 -0500

    STY: fix style with `flake8`, `isort`, and `black`

--- examples/prepare_basler_env.py
+++ examples/prepare_basler_env.py
@@ -1,29 +1,35 @@
-import ophyd_basler
-from ophyd_basler.basler_camera import BaslerCamera
-from ophyd_basler.basler_handler import BaslerCamHDF5Handler
-
 import os
-import numpy as np
-import bluesky.plans as bp
-from bluesky.callbacks import best_effort
+from datetime import datetime
+
+import bluesky.plans as bp  # noqa F401
+import numpy as np  # noqa F401
+from bluesky.callbacks import BestEffortCallback
 from bluesky.run_engine import RunEngine
 from databroker import Broker
 from ophyd.utils import make_dir_tree
-from datetime import datetime
 
-os.environ['PYLON_CAMEMU'] = "1"
+import ophyd_basler
+from ophyd_basler.basler_camera import BaslerCamera  # noqa F401
+from ophyd_basler.basler_handler import BaslerCamHDF5Handler
 
-root_dir = '/tmp/basler'
+os.environ["PYLON_CAMEMU"] = "1"
+
+root_dir = "/tmp/basler"
 _ = make_dir_tree(datetime.now().year, base_path=root_dir)
 
 RE = RunEngine({})
 
-db = Broker.named('local')
+db = Broker.named("local")
 db.reg.register_handler("BASLER_CAM_HDF5", BaslerCamHDF5Handler, overwrite=True)
 RE.subscribe(db.insert)
 
-bec = best_effort.BestEffortCallback()
+bec = BestEffortCallback()
 RE.subscribe(bec)
 
 device_metadata, devices = ophyd_basler.available_devices()
 print(device_metadata)
+
+emulated_basler_camera = BaslerCamera(cam_num=0, verbose=True, root_dir=root_dir, name="basler_cam")
+# (uid,) = RE(bp.count([emulated_basler_camera], num=3))
+# hdr = db[uid]
+# data = np.array(list(hdr.data(field="basler_cam_image", fill=True)))
