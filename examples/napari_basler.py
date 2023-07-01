abce9a20027f7a99ce4ec06c34acac4a5ce9f5f0
Max Rakitin <mrakitin@bnl.gov>
2023-07-01 06:08:54 -0400

    MNT: simplify example/startup script for napary-basler

--- examples/napari_basler.py
+++ examples/napari_basler.py
@@ -1,50 +1,26 @@
 import os
-from datetime import datetime
 
-import bluesky.plans as bp
-import matplotlib.pyplot as plt
 import napari
-import numpy as np
-import pytest
-from bluesky.callbacks import best_effort
-from bluesky.run_engine import RunEngine
-from databroker import Broker
-from ophyd.utils import make_dir_tree
-from skimage import data
+from IPython import get_ipython
 
-import ophyd_basler
-from ophyd_basler.basler_camera import BaslerCamera
-from ophyd_basler.basler_handler import BaslerCamHDF5Handler
-from ophyd_basler.custom_images import get_wandering_gaussian_beam
-from ophyd_basler.utils import plot_images
-
-# RE, db, etc.
-db = Broker.named("temp")
-db.reg.register_handler("BASLER_CAM_HDF5", BaslerCamHDF5Handler, overwrite=True)
-
-RE = RunEngine({})
-bec = best_effort.BestEffortCallback()
-bec.disable_plots()
-RE.subscribe(bec)
-RE.subscribe(db.insert)
-root_dir = "/tmp/basler"
-_ = make_dir_tree(datetime.now().year, base_path=root_dir)
+examples_dir = os.path.dirname(__file__)
+get_ipython().run_line_magic("run", f"-i {examples_dir}/prepare_basler_env.py")
 
 # Napari:
 napari_viewer = napari.Viewer()
+napari_viewer.text_overlay.visible = True
 
 # Basler/ophyd:
-os.environ["PYLON_CAMEMU"] = "1"
+emulated_basler_camera = BaslerCamera(  # noqa: F821
+    cam_num=0, verbose=True, name="basler_cam", viewer=napari_viewer
+)
 
-print(ophyd_basler.available_devices())
-emulated_basler_camera = BaslerCamera(cam_num=0, verbose=True, name="basler_cam", viewer=napari_viewer)
-
-## Generate images:
+# Generate images:
 ny, nx = emulated_basler_camera.image_shape.get()
-WGB = get_wandering_gaussian_beam(nf=256, nx=nx, ny=ny, seed=6313448000)
+WGB = get_wandering_gaussian_beam(nf=256, nx=nx, ny=ny, seed=6313448000)  # noqa: F821
 emulated_basler_camera.set_custom_images(WGB)
-emulated_basler_camera.exposure_time.put(200)
+emulated_basler_camera.exposure_time.put(10)  # in [ms]
 
-# (uid,) = RE(bp.count([emulated_basler_camera], num=100))
+# (uid,) = RE(bp.count([emulated_basler_camera], num=1000))
 # data = emulated_basler_camera._viewer_layer.data
 # print(f"{data = }, {data.shape = }, {data.dtype = }")
--- examples/prepare_basler_env.py
+++ examples/prepare_basler_env.py
@@ -2,19 +2,19 @@ import logging
 import os
 from datetime import datetime
 
-import bluesky.plans as bp  # noqa F401
+import bluesky.plans as bp  # noqa: F401
 import matplotlib.pyplot as plt
-import numpy as np  # noqa F401
+import numpy as np  # noqa: F401
 from bluesky.callbacks.best_effort import BestEffortCallback
 from bluesky.run_engine import RunEngine
 from databroker import Broker
 from ophyd.utils import make_dir_tree
 
 import ophyd_basler
-from ophyd_basler.basler_camera import BaslerCamera  # noqa F401
+from ophyd_basler.basler_camera import BaslerCamera  # noqa: F401
 from ophyd_basler.basler_handler import BaslerCamHDF5Handler
-from ophyd_basler.custom_images import get_wandering_gaussian_beam  # noqa F401
-from ophyd_basler.utils import configure_logger, logger_basler, plot_images  # noqa F401
+from ophyd_basler.custom_images import get_wandering_gaussian_beam  # noqa: F401
+from ophyd_basler.utils import configure_logger, logger_basler, plot_images  # noqa: F401
 
 plt.ion()
 
